dd32b2d01f266e46b459e87c72e99ed9b9a614ea
soura934 <soura934@gmail.com>
2021-01-31 23:19:35 -0500

    Remove the now ignored directory node_modules

--- config/keys.js
+++ config/keys.js
@@ -1,4 +1,4 @@
 module.exports = {
-    mongoURI: 'mongodb+srv://soura934:soura934@cluster0.3mxxv.mongodb.net/test?retryWrites=true&w=majority'
-              //Make sure this is your own unique string
-  }
+    mongoURI: 'mongodb+srv://soura934:soura934@cluster0.3mxxv.mongodb.net/test?retryWrites=true&w=majority',
+    secretOrKey: 'secret' 
+ }
